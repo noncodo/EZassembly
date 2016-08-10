97267f8406bcece26831b14931095847729fa741
Martino <martinalexandersmith@gmail.com>
2016-08-10 13:28:05 +1000

    updated kallisto quantification

--- decorate.bash
+++ decorate.bash
@@ -36,31 +36,43 @@ else mkdir kallisto && cd kallisto
 fi
 
 ## Index trinity fasta for kallisto
-if [[ ! -e kallisto_trinity.index ]]; then 
-	## no SGE 
+if [[ ! -e kallisto_trinity.index ]]; then
+	## no SGE
 	#$KALLISTOBIN index -i kallisto_trinity.index ../$TRINITYFASTA
+
 	## with SGE
-	JID=$( qsub -cwd -terse -l mem_requested=128G,h_vmem=132G -b y -o ./out.sge -V -j y $KALLISTOBIN index -i kallisto_trinity.index ../$TRINITYFASTA ) 
+	CMD="qsub -cwd -S /bin/bash -N K_index -terse -l mem_requested=128G,h_vmem=132G"
+	CMD=$CMD" -b y -o ./out.sge -V -j y ${KALLISTOBIN} index -i kallisto_trinity.index ../${TRINITYFASTA} "
+	JID=$( $CMD )
+	echo "[ SGE ] launched job: "$JID
+	echo "[ SGE ] "$CMD
 fi
 
-#### Run kallisto on all samples 
-
+#### Run kallisto on all samples
 ## No SGE
-# for f in { 1..${NUMSAMPLES} } ; do 
-# 	CMD=" $KALLISTOBIN quant -t ${CPU} ${KSTRAND} -b 50 --bias -i kallisto_trinity.index -o ${file%_*}  $( sed $i'q;d' ../R1.txt ) $( sed $i'q;d' ../R2.txt)"
-# 	$CMD && echo $CMD	
-# done 
-
+# for f in { 1..${NUMSAMPLES} } ; do
+# 	CMD=" $KALLISTOBIN quant -t ${CPU} ${KSTRAND} -b 50 --bias -i kallisto_trinity.index "
+# 	CMD=$CMD" -o ${file%_*}  $( sed $i'q;d' ../R1.txt ) $( sed $i'q;d' ../R2.txt) "
+# 	$CMD && echo $CMD
+# done
 ## With  SGE
-cat >  kallisto_quant.sge << EOF 
- f1=\$( sed \${SGE_TASK_ID}'q;d' ../R1.txt ) 
- f2=\$( sed \${SGE_TASK_ID}'q;d' ../R2.txt ) 
+cat >  kallisto_quant.sge << EOF
+ f1=\$( sed \${SGE_TASK_ID}'q;d' ../R1.txt )
+ f2=\$( sed \${SGE_TASK_ID}'q;d' ../R2.txt )
  outdir=\${f1##*/}
  cmd="${KALLISTOBIN} quant  -i kallisto_trinity.index -t ${CPU} ${KSTRAND} -b 100 --bias -o \${outdir%_*} \${f1} \${f2}"
  \$cmd && echo \$cmd
 EOF
 chmod 755 kallisto_quant.sge
-qsub -cwd -S /bin/bash -N Klsto_Q -S /bin/bash -j y -b y -V -pe smp 6 -l h_vmem=3G,mem_requested=3G -t 1:$( wc -l ../R1.txt | awk '{print $1}' ) ./kallisto_quant.sge
+if [[ ! -z $GROUP_QUOTA ]]; then
+    ACCOUNT=" -P ${GROUP_QUOTA} "
+fi
+if [[ ! -z $JID ]]; then
+    HOLD=" -hold_jid ${JID} "
+fi
+CMD="qsub -cwd ${HOLD} ${ACCOUNT} -S /bin/bash -N Klsto_Q -S /bin/bash -j y -b y -V -pe smp 6"
+CMD=$CMD" -l h_vmem=4G,mem_requested=4G -t 1:$( wc -l ../R1.txt | awk '{print $1}' ) ./kallisto_quant.sge"
+echo "[ SGE ] "$CMD && JID=$( $CMD ) 
 
 
 
